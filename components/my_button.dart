4b215aa29ebb86bb4db97bb410bfecdc2332710f
Dacres0 <da24aby@herts.ac.uk>
2025-12-09 10:58:58 +0000

    Add text property to MyButton widget
    Added a 'text' property to MyButton and updated the build method to use it.

--- components/my_button.dart
+++ components/my_button.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends statelessWidget {
-  const MyButton ({super.key});
+  final String text;
+  const MyButton ({super.key, required this.text,});
 
   @override
   Widget build(BuildContext context) {
     return container(
       decoration: BoxDecoration(
         colour: Theme.of(context).colourScheme.secondary,
+        borderRadius: BorderRadius.circular(8)
+      ), //box decoration
+      padding: const EdgeInsets.all(25),
+      margin: const EdgeInsets.symmetric(horizontal: 25),
       child: Center(
         child: Tex("Button"),
