431cff2ae8835c92cb5e3977fed5282206794139
Dacres0 <da24aby@herts.ac.uk>
2025-12-09 11:24:48 +0000

    Add login form to RegisterPage
    Updated the RegisterPage widget to include a login form with username and password fields, a login button, and a registration prompt.

--- pages/register_page.dart
+++ pages/register_page.dart
@@ -5,4 +5,77 @@ class RegisterPage extends statelessWidget {
 
   @override
   widget build(BuildContext context) {
-    return 
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body:Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //Unity Logo
+            Image.network(
+              'https://i.postimg.cc/GtXyJgkf/Unity-Logo.png',
+              height: 150,
+            ),
+
+            const SizedBox(height: 50),
+
+            //Welcome back message
+            Text(
+              "WELCOME BACK",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                fontSize: 16,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            //Username field
+            MyTextField(
+              hintText: "Username",
+              obscureText: false,
+              controller: _usernameController,
+            ), 
+
+            const SizedBox(height: 10),
+
+            //Password field
+             MyTextField(
+               hintText: "Password",
+               obscureText: true,
+               controller: _pwController,
+            ),
+
+            const SizedBox(height: 25),
+
+            // login button
+            MyButton(
+              text: "Login",
+              onTap: login,
+            ),
+
+            const SizedBox(height: 25),
+
+            //register now
+            Row(
+              mainAxisAlignment: MainAxis Alignment.center,
+              children:
+                Text("Not a member?"),
+                style: 
+                    TextStyle(colour: Theme.of(context).colourScheme.primary),
+               ), // Text
+               Text(
+                 "Register now", 
+                 style: Textstyle(
+                   fontWeight: FontWeight.bold,
+                   colour: Theme.of(context).colourScheme.primary),
+                ),
+        
+            
+
+          ],
+        ),
+      ),
+    );
+  }
+}
