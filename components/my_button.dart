97e21589a8e0ab4cf1c17f8967d5867a0cf64317
Dacres0 <da24aby@herts.ac.uk>
2025-12-09 11:05:56 +0000

    Enhance MyButton with onTap functionality
    Added onTap callback to MyButton for tap handling.

--- components/my_button.dart
+++ components/my_button.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends statelessWidget {
+  final void Function()? onTap;
   final String text;
-  const MyButton ({super.key, required this.text,});
+  
+  const MyButton ({
+    super.key, 
+    required this.text,
+    required this.onTap,
+    });
 
   @override
   Widget build(BuildContext context) {
-    return container(
-      decoration: BoxDecoration(
-        colour: Theme.of(context).colourScheme.secondary,
-        borderRadius: BorderRadius.circular(8)
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          colour: Theme.of(context).colourScheme.secondary,
+          borderRadius: BorderRadius.circular(8)
       ), //box decoration
       padding: const EdgeInsets.all(25),
       margin: const EdgeInsets.symmetric(horizontal: 25),
