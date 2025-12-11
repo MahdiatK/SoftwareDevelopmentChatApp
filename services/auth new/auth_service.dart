785107a9da95396ec5aff929fd0e7be6d770c011
MK <163010589+MahdiatK@users.noreply.github.com>
2025-12-11 17:18:45 +0000

    Refactor AuthService and add user info saving
    Refactored signInWithEmailPassword method to improve readability and added user info saving to Firestore.

--- services/auth new/auth_service.dart
+++ services/auth new/auth_service.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 
 class AuthService {
   //instance of auth and firestore 
@@ -10,7 +9,20 @@ class AuthService {
   //sign in
   Future<UserCredential> signInWithEmailPassword(String email, String password) async {
     try {
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password, );
+      // sign user in
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email, 
+        password: password, 
+      );
+
+      // save user info if it doesn't already exist
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          'uid': userCredential.user!.uid,
+          'email':email,
+        },
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
@@ -30,7 +42,8 @@ class AuthService {
       // save user info in separate document
       _firestore.collection("Users").doc(userCredential.user!.uid).set(
         {
-          
+          'uid': userCredential.user!.uid,
+          'email':email,
         },
       );
 
