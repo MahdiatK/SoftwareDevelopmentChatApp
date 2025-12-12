5bb19a9260642dc05008d1a72bf57c5840e5e641
Dacres0 <da24aby@herts.ac.uk>
2025-12-12 12:22:00 +0000

    Implement user input for chat messages
    Added a user input widget for message entry.

--- pages/chat_page.dart
+++ pages/chat_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:unity_main/components/textfield.dart';
 import 'package:unity_main/services/auth/auth_service.dart';
 import 'package:unity_main/services/chat/chat_service.dart';
 
@@ -76,5 +77,25 @@ class ChatPage extends StatelessWidget {
       Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
 
       return Text(data["message"]);
+    }
+
+    //build message input 
+    Widget _buildUserInput() {
+      return Row(
+        children: [
+          // textfield should take up most of the space
+          Expanded(
+            child: MyTextField(
+              controller: _messageController,
+              hintText: "Enter your message",
+              obscureText: false,
+            ),
+          ),
+
+          // send button
+          IconButton(onPressed: sendMessage, icon: Icon(Icons.arrow_upward),
+          ),
+        ],
+      );
     }
   }
