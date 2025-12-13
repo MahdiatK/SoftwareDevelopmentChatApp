bc419b00426ca49e1300df5c679aebff4d5788e8
Sayf <sb24afs@herts.ac.uk>
2025-12-13 18:34:12 +0000

    Update chat_bubble.dart changing colors

--- components/chat_bubble.dart
+++ components/chat_bubble.dart
@@ -14,7 +14,7 @@ class ChatBubble extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        color: isCurrentUser ? Color.fromARGB(255, 208, 222, 17) : Colors.grey.shade500,
+        color: isCurrentUser ? const Color.fromARGB(255, 104, 95, 14) : Colors.grey.shade500,
         borderRadius: BorderRadius.circular(12),
       ),
       padding: const EdgeInsets.all(16),
