cad563657fddff37a98237162571e433f0bc86b0
Sayf <sb24afs@herts.ac.uk>
2025-12-13 18:26:41 +0000

    Update settings_page.dart

--- pages/settings_page.dart
+++ pages/settings_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:unity_main/brightness/brightness_provider.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -12,6 +15,30 @@ class SettingsPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         foregroundColor: Colors.grey,
         elevation: 0,
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        margin:const EdgeInsets.all(25),
+        padding:const EdgeInsets.all(16),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //Dark Mode
+            const Text("Dark Mode"),
+        
+            //Switch Toggle
+            CupertinoSwitch(
+              value:
+                 Provider.of<BrightnessProvider>(context, listen: false).isDarkMode,
+              onChanged: (value) =>
+                 Provider.of<BrightnessProvider>(context, listen: false)
+                     .toggleTheme(),
+            ),
+          ],
+        ),
       ),
     );
   }
