0b5ef478527eb439857b6284ea5a99fdc1cd0a11
Sayf <sb24afs@herts.ac.uk>
2025-12-13 16:25:38 +0000

    Update main.dart to allow light and dark mode provider

--- main.dart
+++ main.dart
@@ -1,13 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'package:unity_main/auth/auth_gate.dart';
-import 'package:unity_main/brightness/light_mode.dart';
+import 'package:unity_main/brightness/brightness_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:unity_main/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); 
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => BrightnessProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -18,7 +24,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,  //This makes the program not show the debug banner when testing/running
       home: AuthGate(),                  //App begins on the login page
-      theme: lightMode,
+      theme: Provider.of<BrightnessProvider>(context).themeData,
     );
   }
 }
